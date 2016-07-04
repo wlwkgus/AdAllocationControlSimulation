ae2998db93ce2a7d228e08faf54454db816757df
Tony Kim <wlwkgus@nate.com>
2016-07-04 18:39:03 +0900

    modified

--- PISimulator.py
+++ PISimulator.py
@@ -3,16 +3,15 @@ from Simulator import Simulator
 import random
 
 class PISimulator(Simulator):
-	Kp = -300
-	Ki = -5
+	Kp = -0.0005
+	Ki = -4
 	OUTPUT_NAME = 'output2.csv'
 	INTEGRAL_OFFSET_RATIO = 0.01
 	errorIntegral = 0
 	# INTEGRAL 에러 누적으로 windup 방지를 위해 만듦
 	INTEGRAL_ERROR_LIMIT = 1
 	def IsCap(self):
-		self.IntegralError()
-		cap = 100 + (self.Kp * (self.budget - self.spent) / self.budget) + self.Ki * self.errorIntegral / self.budget
+		cap = 100 + (self.Kp * (self.budget - self.spent)) + self.Ki * self.INTEGRAL_ERROR_LIMIT
 		if cap < 0:
 			cap = 0
 		elif cap > 100:
@@ -25,29 +24,3 @@ class PISimulator(Simulator):
 		if random.randrange(100) < cap:
 			return True
 		return False
-
-	def IntegralError(self):
-		if self.errorIntegral >= self.INTEGRAL_ERROR_LIMIT * self.budget:
-			if (self.budget - self.timeSpent[self.now]) > 0:
-				return
-		if self.errorIntegral <= -self.INTEGRAL_ERROR_LIMIT * self.budget:
-			if (self.budget - self.timeSpent[self.now]) < 0:
-				return
-
-		self.errorIntegral += self.budget - self.timeSpent[self.now]
-
-
-		# for i in xrange(self.now):
-		# 	if errorSum >= self.INTEGRAL_ERROR_LIMIT * self.budget:
-		# 		if (self.budget - self.timeSpent[i]) > 0:
-		# 			continue
-		# 	if errorSum <= -self.INTEGRAL_ERROR_LIMIT * self.budget:
-		# 		if (self.budget - self.timeSpent[i]) < 0:
-		# 			continue
-		# 	errorSum += self.budget - self.timeSpent[i]
-		# result = errorSum / self.budget
-		# if result >= self.INTEGRAL_ERROR_LIMIT:
-		# 	return self.INTEGRAL_ERROR_LIMIT
-		# if result <= -self.INTEGRAL_ERROR_LIMIT:
-		# 	return -self.INTEGRAL_ERROR_LIMIT
-		# return errorSum / self.budget
--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from CurrentSimulator import CurrentSimulator
 from PISimulator import PISimulator
 
 if __name__ == '__main__':
-	budget = 150000
+	budget = 200000
 	unitPrice = 40
 
 	currentSimulator = CurrentSimulator(budget=budget, unitPrice=unitPrice)
 	piSimulator = PISimulator(budget=budget, unitPrice=unitPrice)
 
-	for _ in xrange(7500):
+	for _ in xrange(75000):
 		currentSimulator.AdAllocation()
 		currentSimulator.DepositBudget()
 		piSimulator.AdAllocation()
